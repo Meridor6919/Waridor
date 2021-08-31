19115de151032b09cfde5018d19d29c6111a6bb7
Michał Balicki <michalbalicki@o2.pl>
2021-09-01 00:46:36 +0200

    Improved input handling mechanism.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,88 @@
 #[macro_use]
 extern crate glium;
 
+#[allow(unused_imports)]
+use glium::{glutin, Surface};
 use std::io::Cursor;
+
+enum MoveDirections{
+    Left,
+    Right,
+    None
+}
+struct Player{
+    moving : MoveDirections,
+    pos : f32,
+}
+impl Player{
+    fn input(&mut self, input : glutin::event::KeyboardInput) -> bool{
+        let f = input.virtual_keycode.unwrap();
+        match f{
+            //Game related input
+            glutin::event::VirtualKeyCode::Left =>{
+                if input.state == glutin::event::ElementState::Pressed {self.moving = MoveDirections::Left} else {self.moving = MoveDirections::None} 
+                return false;
+            },
+            glutin::event::VirtualKeyCode::Right =>{
+                if input.state == glutin::event::ElementState::Pressed {self.moving = MoveDirections::Right} else {self.moving = MoveDirections::None} 
+                return false;
+            },
+            glutin::event::VirtualKeyCode::LControl =>{
+                return false;
+            },
+            _=>return true,
+        }
+    }
+    fn update(&mut self) -> (){
+        match self.moving{
+            MoveDirections::Left =>{
+                self.pos -= 0.02
+            },
+            MoveDirections::Right =>{
+                self.pos += 0.02
+            },
+            _=> return,
+        }
+    }
+}
+
+struct Game{
+}
+impl Game{
+    fn input(&self, input : glutin::event::KeyboardInput) -> bool{
+        let f = input.virtual_keycode.unwrap();
+        match f{
+            //Game related input
+            glutin::event::VirtualKeyCode::Return =>{
+                return false;
+            },
+            glutin::event::VirtualKeyCode::R =>{
+                return false;
+            },
+            _=>return true,
+        }
+    }
+}
+struct Shop{
+}
+impl Shop{
+    fn input(&self, input : glutin::event::KeyboardInput) -> bool{
+        let f = input.virtual_keycode.unwrap();
+        match f{
+            //Game related input
+            glutin::event::VirtualKeyCode::Up =>{
+                return false;
+            },
+            glutin::event::VirtualKeyCode::Down =>{
+                return false;
+            },
+            glutin::event::VirtualKeyCode::Return =>{
+                return false;
+            },
+            _=>return true,
+        }
+    }
+}
 
 fn main() {
     #[allow(unused_imports)]
@@ -26,10 +107,10 @@ fn main() {
 
     implement_vertex!(Vertex, position, tex_coords);
 
-    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
-    let vertex2 = Vertex { position: [ -0.5,  0.5], tex_coords: [0.0, 1.0] };
-    let vertex3 = Vertex { position: [ 0.5, 0.5], tex_coords: [1.0, 1.0] };
-    let vertex4 = Vertex { position: [ 0.5, -0.5], tex_coords: [1.0, 0.0] };
+    let vertex1 = Vertex { position: [-0.0625, -0.0625], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ -0.0625,  0.0625], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.0625, 0.0625], tex_coords: [1.0, 1.0] };
+    let vertex4 = Vertex { position: [ 0.0625, -0.0625], tex_coords: [1.0, 0.0] };
     
     let shape = vec![vertex1, vertex2, vertex4, vertex3];
 
@@ -60,31 +141,32 @@ fn main() {
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
-    let mut t = -0.5;
-    event_loop.run(move |event, _, control_flow| {
+    let mut player = Player{moving : MoveDirections::None, pos : 0.5};
+    let mut shop =Shop{};
+    let mut game = Game{};
 
+    event_loop.run(move |event, _, control_flow| {
+        
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
                 },
-                glutin::event::WindowEvent::KeyboardInput{ input, .. } => match input.virtual_keycode{
-                    Some(x) =>{
-                         if x == glutin::event::VirtualKeyCode::Left
-                         {
-                             *control_flow = glutin::event_loop::ControlFlow::Exit;
-                             return;
-                         }
-                    },
-                    _=> return,
-                },
-                glutin::event::WindowEvent::MouseInput{ button,.. } => match button{
-                    glutin::event::MouseButton::Right =>{
-                        *control_flow = glutin::event_loop::ControlFlow::Exit;
-                        return;
-                    },
-                    _=> return,
+                glutin::event::WindowEvent::KeyboardInput{ input, .. } => {
+                    if game.input(input)
+                    {
+                        if shop.input(input)
+                        {
+                            if player.input(input)
+                            {
+                                return;
+                            }
+                        }
+                    }
                 },
                 _ => return,
             },
@@ -95,17 +177,7 @@ fn main() {
             },
             _ => return,
         }
-
-        let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
-        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
-
-        // we update `t`
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
+        player.update();
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
 
@@ -114,7 +186,7 @@ fn main() {
                 [1.0, 0.0, 0.0, 0.0],
                 [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
-                [ t , 0.0, 0.0, 1.0f32],
+                [ player.pos , 0.0, 0.0, 1.0f32],
             ],
             tex: &texture,
         };
@@ -122,5 +194,7 @@ fn main() {
         target.draw(&vertex_buffer, &indices, &program, &uniforms,
                     &Default::default()).unwrap();
         target.finish().unwrap();
+
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
     });
 }
